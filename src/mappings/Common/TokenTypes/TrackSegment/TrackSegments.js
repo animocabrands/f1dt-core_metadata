4e27e4375cd8f633e0900099dbde5dcd73a25f99
Andy Siu <andys@animocabrands.com>
2020-11-27 15:52:37 +0800

    - Updated TrackSegment.js description refractor into one variable

--- src/mappings/Common/TokenTypes/TrackSegment/TrackSegments.js
+++ src/mappings/Common/TokenTypes/TrackSegment/TrackSegments.js
@@ -1,20 +1,21 @@
 //The key is based on rarity
+const monacaTrackDesc = "Home to the illustrious FORMULA 1 GRAND PRIX DE MONACO (“Monaco Grand Prix”),  the Circuit de Monaco is a track laid out on the city streets of Monte Carlo and La Condamine in the principality of Monaco, one of the most glamorous and prestigious locations in motorsport. The Monaco Grand Prix was established in 1929 and is considered one of the world’s most important automobile races.";
 let MonacoTrackSegments = {
     '0' : { 
         name: 'Grand Prix de Monaco',
-        description: 'Home to the illustrious FORMULA 1 GRAND PRIX DE MONACO (“Monaco Grand Prix”),  the Circuit de Monaco is a track laid out on the city streets of Monte Carlo and La Condamine in the principality of Monaco, one of the most glamorous and prestigious locations in motorsport. The Monaco Grand Prix was established in 1929 and is considered one of the world’s most important automobile races.'
+        description: monacaTrackDesc
     },
     '1' : {
         name: 'Grand Prix de Monaco',
-        description: 'Home to the illustrious FORMULA 1 GRAND PRIX DE MONACO (“Monaco Grand Prix”),  the Circuit de Monaco is a track laid out on the city streets of Monte Carlo and La Condamine in the principality of Monaco, one of the most glamorous and prestigious locations in motorsport. The Monaco Grand Prix was established in 1929 and is considered one of the world’s most important automobile races.'
+        description: monacaTrackDesc
     },
     '2' : {
         name: 'Grand Prix de Monaco',
-        description: 'Home to the illustrious FORMULA 1 GRAND PRIX DE MONACO (“Monaco Grand Prix”),  the Circuit de Monaco is a track laid out on the city streets of Monte Carlo and La Condamine in the principality of Monaco, one of the most glamorous and prestigious locations in motorsport. The Monaco Grand Prix was established in 1929 and is considered one of the world’s most important automobile races.'
+        description: monacaTrackDesc
     },
     '4' : {
         name: 'Grand Prix de Monaco',
-        description: 'Home to the illustrious FORMULA 1 GRAND PRIX DE MONACO (“Monaco Grand Prix”),  the Circuit de Monaco is a track laid out on the city streets of Monte Carlo and La Condamine in the principality of Monaco, one of the most glamorous and prestigious locations in motorsport. The Monaco Grand Prix was established in 1929 and is considered one of the world’s most important automobile races.'
+        description: monacaTrackDesc
     }
 }
 
