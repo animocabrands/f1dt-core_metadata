0bbeb877ff648b961041763cd33b2ae5553dcd70
Lucas Tseng <lucast@animocabrands.com>
2021-02-11 13:19:46 +0800

    Add script to generate legendary, epic, rare Monaco track tokens

--- test/mintNFTtest/mintTrackTokens.js
+++ test/mintNFTtest/mintTrackTokens.js
@@ -1,4 +1,5 @@
 const { getCoreMetadata, getFullMetadata } = require('../../src/utils/metadata');
+const { ById: TrackById } = require('../../src/mappings/Common/Attributes/Track');
 const { createTokenId } = require('../../src/utils/ids');
 const BigInteger = require('big-integer');
 
@@ -26,3 +27,75 @@ try {
 } catch (e) {
     console.error(e);
 }
+
+const rarityToSegmentIdRangeMap = {
+    '1': { // Monaco
+        '1': { // Legendary
+            min: 'B'.charCodeAt(0),
+            max: 'C'.charCodeAt(0),
+        },
+        '2': { // Epic
+            min: 'D'.charCodeAt(0),
+            max: 'H'.charCodeAt(0),
+        },
+        '4': { // Rare
+            min: 'I'.charCodeAt(0),
+            max: 'P'.charCodeAt(0),
+        },
+    }
+};
+
+const rarityToTrackCountMap = {
+    '1': { // Monaco
+        '1': 12, // Legendary
+        '2': 20, // Epic
+        '4': 25, // Rare
+    }
+};
+
+const rarityToEarningsMap = {
+    '1': '' + BigInteger(1*10**7),
+    '2': '' + BigInteger(25*10**5),
+    '4': '' + BigInteger(125*10**4),
+};
+
+const createTrackTokenId = (rarityId, country, trackId, segmentId, zoneId, earnings) => {
+    const tokenId = createTokenId({
+        type: 'Track',
+        subType: 'None',
+        season: '0',
+        rarity: rarityId,
+        country: country,
+        trackId: trackId,
+        trackSegment: {
+            segmentId: segmentId,
+            zoneId: zoneId,
+            earnings: earnings
+        },
+    });
+
+    return tokenId;
+}
+
+describe('Tracks', function() {
+    describe("Generate token IDs", function() {
+        it("Build tokens", function() {
+            const trackIds = Object.keys(TrackById).filter(key => key !== '0' && key !== '2');
+            trackIds.forEach(trackId => {
+                const rangeMap = new Map(Object.entries(rarityToSegmentIdRangeMap[trackId]));
+                rangeMap.forEach((range, rarity) => {
+                    const earnings = rarityToEarningsMap[rarity];
+                    for (let i = range.min; i <= range.max; i++) {
+                        const segmentId = '' + i;
+                        for (let j = 1; j <= rarityToTrackCountMap[trackId][rarity]; j++) {
+                            const zoneId = '' + j;
+                            const country = TrackById[trackId].countryId.country;
+                            const tokenId = createTrackTokenId(rarity, country, trackId, segmentId, zoneId, earnings);
+                            console.log(tokenId);
+                        }
+                    }
+                });
+            });
+        });
+    });
+});
